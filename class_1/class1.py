10983790baf946a82922ee6136e48748d8e45555
XT Joe <31270922+FabregasZhou@users.noreply.github.com>
2019-07-26 16:57:30 +0800

    Add files via upload

---
+++ class_1/class1.py
@@ -0,0 +1,15 @@
+print('Hello world!')
+
+print('Hello','world！')
+
+name = 'Joe'
+print('Hello', name, '!')
+
+name = 'Joe'
+score = 60
+print('Hello', name, '!', 'your score is', score)
+
+name = input('Please input your name:')
+print('Hello', name, '!')
+
+print('I\'m fine.')
